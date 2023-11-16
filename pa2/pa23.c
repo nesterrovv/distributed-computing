87b0728782fbc1e342bdfdc6d7caaa8f8eee7d6e
nesterrovv <nesterov.iv2018@gmail.com>
2023-11-16 19:06:44 +0300

    feat: lab work #2 code smallfixes & cleanup

--- pa2/pa23.c
+++ pa2/pa23.c
@@ -5,6 +5,7 @@
 #include <errno.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdarg.h>
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <dlfcn.h>
@@ -14,14 +15,79 @@
 #include "ipc.h"
 #include "pa2345.h"
 
-static const char* const manual =
-        "Sample of our IPC library using in bank robbery simulation\n"
-        "Execution instruction: -p NUMBER_OF_PROCESSES will start simulation of balances for this processes\n"
-        "ATTENTION: NUMBER OF PROCESSES SHOULD BE FROM SEGMENT [0;10]";
-
 pid_t pid_of_processes[MAXIMAL_PROCESSES_NUMBER + 1];
 balance_t initial_balances_of_clients[MAXIMAL_PROCESSES_NUMBER + 1];
 
+static FILE* log_file_for_info;
+static FILE* log_file_for_pipes;
+
+void log_formatted_arguments_without_file(const char* format, va_list args) {
+    vprintf(format, args);
+}
+
+void log_formatted_errors_without_file(const char* format, ...) {
+    va_list error_arguments;
+    va_start(error_arguments, format);
+    fprintf(stderr, format, error_arguments);
+    va_end(error_arguments);
+}
+
+void log_formatted_arguments_to_file(FILE* file, const char *format, va_list args) {
+    if (file == NULL) {
+        fprintf(stderr, "Cannot write logs with format \"%s\"", format);
+        return;
+    }
+    vfprintf(file, format, args);
+}
+
+void log_info(const char *format, ...) {
+    va_list logging_arguments;
+    va_start(logging_arguments, format);
+    log_formatted_arguments_without_file(format, logging_arguments);
+    log_formatted_arguments_to_file(log_file_for_info, format, logging_arguments);
+    va_end(logging_arguments);
+}
+
+void log_starting_info() {
+    pid_t pid = getpid();
+    pid_t parent_pid = getppid();
+    balance_t log_balance = message.balance_history.s_history[message.balance_history.s_history_len - 1].s_balance;
+    log_info(log_started_fmt, get_physical_time(), message.message_id, pid, parent_pid, log_balance);
+}
+
+void do_pre_run_logging() {
+    if ((log_file_for_info = fopen(events_log, "w")) == NULL)
+        log_formatted_errors_without_file("File cannot be opened.", events_log);
+
+    if ((log_file_for_pipes = fopen(pipes_log, "w")) == NULL)
+        log_formatted_errors_without_file("File cannot be opened.", pipes_log);
+}
+
+void log_running_processes() {
+    timestamp_t phys_time = get_physical_time();
+    log_info(log_received_all_started_fmt, phys_time, message.message_id);
+}
+
+void log_finished_processes() {
+    timestamp_t phys_time = get_physical_time();
+    log_info(log_received_all_done_fmt, phys_time, message.message_id);
+}
+
+void log_info_about_pipes(const char *format, ...) {
+    va_list logging_arguments;
+    va_start(logging_arguments, format);
+    log_formatted_arguments_to_file(log_file_for_pipes, format, logging_arguments);
+    log_formatted_arguments_without_file(format, logging_arguments);;
+    va_end(logging_arguments);
+}
+
+void log_pipes_is_ready(int source_id, int destination_id) {
+    log_info_about_pipes("Pipe from process %1d to %1d OPENED\n", source_id, destination_id);
+}
+
+void log_transmit_info(local_id source_id, local_id destination_id, balance_t balance) {
+    log_info(log_transfer_out_fmt, get_physical_time(), source_id,balance, destination_id);
+}
 
 void renew_metrics(int state_sum, timestamp_t time) {
     if (time <= MAX_T) {
@@ -82,13 +148,13 @@ void connect_to_children_processes(local_id parrend_process_id) {
 }
 
 
-void share_handler(Message* source_message, Message* content) {
+void share_handler(Message* content) {
     timestamp_t current_physical_time = get_physical_time();
     Transfer* pointer_to_transfer = (Transfer*) content -> s_payload;
     TransferOrder transfer_order = pointer_to_transfer -> transfer_order;
     if (message.message_id == transfer_order.s_dst) {
         renew_metrics(transfer_order.s_amount, current_physical_time);
-        //transfer_in_fmt(transfer_order.s_src, transfer_order.s_dst, transfer_order.s_amount);
+        log_info(log_transfer_in_fmt, transfer_order.s_src, transfer_order.s_dst, transfer_order.s_amount);
         Message ack_message = {
                 .s_header = {
                         .s_magic = MESSAGE_MAGIC,
@@ -102,7 +168,7 @@ void share_handler(Message* source_message, Message* content) {
     }
     else {
         renew_metrics(-transfer_order.s_amount, current_physical_time);
-        //transfer_out_fmt(transfer_order.s_src, transfer_order.s_dst, transfer_order.s_amount);
+        log_transmit_info(transfer_order.s_src, transfer_order.s_dst, transfer_order.s_amount);
         pointer_to_transfer -> message_id = message.message_id;
         transfer(
                 pointer_to_transfer,
@@ -123,7 +189,6 @@ int release_cs(const void* source) {
             history -> s_history[balance_history -> s_id - 1] = *balance_history;
         }
         else {
-            //printf("History reading process has failed.\n");
             return IPC_MESSAGE_STATUS_ERROR_CANNOT_READ;
         }
     }
@@ -141,7 +206,7 @@ int main(int argc, char * argv[]) {
     if (has_enough_args && has_flag && strtol_is_valid) {
         children_process_number = strtol(argv[2], NULL, 10);
         if (children_process_number >= MAXIMAL_PROCESSES_NUMBER) {
-            //log_errorfmt(manual);
+            log_formatted_errors_without_file("Processes number is out of bounds.");
             return 1;
         } else {
             for (int current_child = 1; current_child <= children_process_number; current_child += 1) {
@@ -153,16 +218,15 @@ int main(int argc, char * argv[]) {
             }
         }
     } else {
-        //log_errorfmt(manual);
+        log_formatted_errors_without_file("Invalid program running options.");
         return 1;
     }
-
-//    if (children_process_number >= MAXIMAL_PROCESSES_NUMBER) {
-//        //log_errorfmt(manual);
-//        return 1;
-//    }
+    if (children_process_number >= MAXIMAL_PROCESSES_NUMBER) {
+        log_formatted_errors_without_file("Processes number is out of bounds.");
+        return 1;
+    }
     number_of_processes = children_process_number + 1;
-    //log_init();
+    do_pre_run_logging();
     int failed_to_open_pipes_exit_code = 2;
     for (size_t i = 0; i < number_of_processes; i += 1) {
         for (size_t j = 0; j < number_of_processes; j += 1) {
@@ -170,13 +234,13 @@ int main(int argc, char * argv[]) {
                 continue;
             } else {
                 int fd[2]; pipe(fd);
-                if (fcntl(fd[0], FSETFL, fcntl(fd[0], F_GETFL, 0) | O_NONBLOCK) < 0
+                if (fcntl(fd[0], F_SETFL, fcntl(fd[0], F_GETFL, 0) | O_NONBLOCK) < 0
                     || fcntl(fd[1], F_SETFL, fcntl(fd[1], F_GETFL, 0) | O_NONBLOCK)) {
                     exit(failed_to_open_pipes_exit_code);
                 }
                 read_matrix[i][j] = fd[0];
                 write_matrix[i][j] = fd[1];
-                pipe_opened(i, j);
+                log_pipes_is_ready(i, j);
             }
         }
     }
@@ -202,49 +266,41 @@ int main(int argc, char * argv[]) {
         } else if (child_pid > 0) {
             message = (ipc_message) {.message_id = PARENT_ID,};
             pid_of_processes[current_id] = child_pid;
-        } //else {
-            //log_errorfmt("Error: Cannot create process. Parrent: %d", pid_of_processes[PARENT_ID]);
-        //}
+        } else {
+            log_formatted_errors_without_file("Process from parent %d not created.", pid_of_processes[PARENT_ID]);
+        }
     }
 
     pipes_cleanup();
     ipc_message_status status;
 
     if (message.message_id == PARENT_ID) {
-        //started();
+        log_starting_info();
         Message content;
         receive_from_all_children(&message, &content, children_process_number);
-        //received_all_started();
-
+        log_running_processes();
         Transfer msg_transfer = {.message_id = message.message_id,};
         bank_robbery(&msg_transfer, children_process_number);
-
         status = send_stop_to_all(&message);
         if (status != IPC_MESSAGE_STATUS_SUCCESS) {
-            //log_errorfmt("Error: failed to send multicast from %d. Code: %d\n", local.ipc_id, status);
+            log_formatted_errors_without_file("Multicast sending from %d. finished with no-zero code: %d\n", message.message_id, status);
         }
-        // Receive ALL DONE
         receive_from_all_children(&message, &content, children_process_number);
-        //received_all_done();
-
-
-        // Receive ALL HISTORY
+        log_finished_processes();
         AllHistory all_history = {.s_history_len = children_process_number,};
         status = release_cs(&all_history);
-
-        if (status != IPC_MESSAGE_STATUS_SUCCESS) {
-            //log_errorfmt("Error: failed to receive message. Code: %d\n", status);
-        }
-        else {
+        if (status == IPC_MESSAGE_STATUS_SUCCESS) {
             print_history(&all_history);
+        } else {
+            log_formatted_errors_without_file("Message for receiving for current process finished with with no-zero code %d\n", status);
         }
     }
     else {
         Message content;
         send_started_to_all(&message);
-        //started();
+        log_starting_info();
         receive_from_all_children(&message, &content, children_process_number);
-        //received_all_started();
+        log_running_processes();
         int16_t type = -1;
         local_id children_processes_left = children_process_number - 1;
         for (;;) {
@@ -253,7 +309,7 @@ int main(int argc, char * argv[]) {
             } else {
                 type = receive_any(&message, &content);
                 if (type == TRANSFER) {
-                    share_handler(&message, &content);
+                    share_handler(&content);
                 } else if (type == DONE) {
                     children_processes_left--;
                 } else if (type == STOP) {
@@ -261,16 +317,16 @@ int main(int argc, char * argv[]) {
                 }
             }
         }
+        log_finished_processes();
         type = -1;
         while(children_processes_left > 0) {
             type = receive_any(&message, &content);
             if (type == TRANSFER) {
-                share_handler(&message, &content);
+                share_handler(&content);
             } else if (type == DONE) {
                 children_processes_left--;
             }
         }
-        //received_all_done();
         timestamp_t current_physical_time = get_physical_time();
         renew_metrics(0, current_physical_time);
         Message msg_hisory = {
@@ -288,10 +344,9 @@ int main(int argc, char * argv[]) {
         );
         status = send(&message, PARENT_ID, &msg_hisory);
         if (status != IPC_MESSAGE_STATUS_SUCCESS) {
-            //log_errorfmt("Error: failed to send multicast from %d. Code: %d\n", local.ipc_id, status);
+            log_formatted_errors_without_file("Multicast sending from %d. finished with no-zero code: %d\n", message.message_id, status);
         }
     }
     connect_to_children_processes(PARENT_ID);
-    //log_end();
     return 0;
 }
